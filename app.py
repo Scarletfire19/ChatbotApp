6444c4baf2aadb5a1c8faf300930d78708b045c2
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2024-06-04 14:37:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,6 @@ import pandas as pd
 import base64
 
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-    
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Ask Gurudev Anything Chatbot")
 
